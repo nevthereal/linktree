7961a711dec411b3df6a7fa6f9950ee100b230a8
Neville Brem <bremneville@gmail.com>
2024-03-31 11:49:10 +0200

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercelStatic(),
 });
