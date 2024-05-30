f351664c7a427e41b1050d1d17e04445a34d8a6b
Neville Brem <bremneville@gmail.com>
2024-05-30 10:33:15 +0200

    finished schema

--- db/config.ts
+++ db/config.ts
@@ -1,15 +1,13 @@
 import { column, defineDb, defineTable } from "astro:db";
 
-export default defineDb({
-  tables: {
-    Link,
-  },
-});
-
 const Link = defineTable({
   columns: {
     desc: column.text({ primaryKey: true }),
     link: column.text(),
     icon: column.text(),
   },
+});
+
+export default defineDb({
+  tables: { Link },
 });
