11c0199526540774a6341d8b5167159be2c33c57
Neville Brem <bremneville@gmail.com>
2024-11-24 20:40:01 +0100

    lezzgo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,6 @@ import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: vercel(),
   output: "static",
 });
